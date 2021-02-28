3217920960b9df89ec5a5ea0e930b4c09d38e87c
Joao Paulo Lindgren <joaopozo@gmail.com>
2021-02-28 01:58:41 -0300

    add app bootstrap

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('running');
+import { SetupServer } from './server';
+import config from 'config';
+
+const port: number = config.get('App.port');
+
+(async () => {
+  console.info('Bootstrapping application');
+  const server = new SetupServer(port);
+  await server.init();
+  server.start();
+})();
