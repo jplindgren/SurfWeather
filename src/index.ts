0ec1bbdae787aa828af014c97513bfbd86279c4b
Joao Paulo Lindgren <joaopozo@gmail.com>
2021-03-03 18:37:44 -0300

    add heroku deploy

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import { SetupServer } from './server';
 import config from 'config';
 
 const port: number = config.get('App.port');
+console.log('choosen port -> ', port);
 
 (async () => {
   console.info('Bootstrapping application');
+
   const server = new SetupServer(port);
   await server.init();
   server.start();
